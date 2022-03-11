7b423f44594c9a4cb1e9869405602f5bb1a2ba7e
Delta203 <ddelta203@gmail.com>
2022-03-11 03:22:04 +0100

    Add files via upload

---
+++ hashfunction.py
@@ -0,0 +1,66 @@
+"""
+Hash Function
+Usage: from hashfunction import Hash
+"""
+
+class Hash:
+    def __init__(self): pass
+    
+    def hexHash(self, st: str, printBlocks: bool=False, printBitHash: bool=False) -> str:
+        """
+        generate hash value
+        output: hex 32 byte
+        
+        printBlocks : bool | debug binary value of string
+        printBitHash : bool | debug bithash
+        """
+        st_len = len(st)
+        blockLen = 512 # 512 for 32 byte hash
+        minBlockAmount = blockLen * 3
+        primeKey = 1373*st_len # key modulus high prime number avoiding quick collisions
+        bitString = str(self.__stringToBinary(st)) + str(self.__stringToBinary(st))[::-1]
+        if len(bitString) < minBlockAmount: bitString = (bitString*((minBlockAmount//len(bitString))+1))[:minBlockAmount]
+        
+        trim = [bitString[blockLen*x:blockLen*(x+1)] for x in range(len(bitString)//blockLen)]
+        trimMod = [str(str(self.__stringToBinary(str(int(x) % primeKey))) * (blockLen//len(str(self.__stringToBinary(str(int(x) % primeKey))))+1))[:blockLen] for x in trim]
+        trimfull = trim + trimMod
+        
+        cachebitHash = "0" * blockLen
+        c = 0
+        for i in trimfull:
+            if printBlocks: print("Block " + str(c) + "     :", i, len(i))
+            cachebitHash = self.__XOR(cachebitHash, i)
+            c+=1
+        bitHash = self.__XOR(cachebitHash[:blockLen//2], cachebitHash[blockLen//2:]) # compress 512 bit to 256 bits
+        
+        if printBitHash: print("bithash     :", bitHash, len(bitHash))
+        hexHash = self.__numToHex(int(bitHash))
+        
+        print(st_len)
+        
+        return hexHash
+    
+    def __stringToBinary(self, st: str) -> int:
+        return int("".join(format(ord(i), "08b") for i in st))
+        
+    def __numToHex(self, num: int) -> str:
+        """ num: int | has to be bit format """
+        return hex(int(str(num), 2))
+    
+    def __XOR(self, stnum1: str, stnum2: str) -> int:
+        """
+        xor number function
+        len(stnum1) == len(stnum2)
+        
+        Returns: int | bit
+        """
+        result = ""
+        for i in range(len(stnum1)):
+            if i == 0: 
+                result+="1"
+                continue
+            if stnum1[i] == "0" and stnum2[i] == "0": result+="0"
+            elif stnum1[i] == "1" and stnum2[i] == "0": result+="1"
+            elif stnum1[i] == "0" and stnum2[i] == "1": result+="1"
+            elif stnum1[i] == "1" and stnum2[i] == "1": result+="0"
+        return result
---
+++ launcher.py
@@ -0,0 +1,89 @@
+import sys, time
+from hashfunction import Hash
+
+# usage: python hashfunction.py <words>
+start_time = time.time()
+
+w = ""
+for i in range(1, len(sys.argv)):
+    w = w + " " + sys.argv[i]
+w = w[1:]
+
+hashFunction = Hash()
+
+if w.startswith("-fetch"):
+    fname = sys.argv[2]
+    ftname = sys.argv[3]
+    maxThreads = 50000
+    try: maxThreads = int(sys.argv[4])
+    except: pass
+    print("Fetch every hash for passwordlist: " + fname + " | maxThreads: " + str(maxThreads))
+    time.sleep(1)
+    
+    file = open(fname, "r", encoding="utf-8")
+    lines = file.readlines()
+    file.close()
+    
+    file = open(ftname, "a", encoding="utf-8")
+    c = 1
+    for val in lines:
+        val = val.replace("\n", "")
+        try:
+            val_h = hashFunction.hexHash(val)
+            print(c, val, val_h)
+            file.write(val_h + "\n")
+        except:
+            print(c, val, "!no hash")
+            file.write("no hash: [" + val + "]\n")
+        if c == maxThreads: break
+        c+=1
+    file.close()
+    print("Fetch done. " + str(c-1) + " strings hashed")
+elif w.startswith("-range"):
+    ftname = sys.argv[2]
+    start = 10
+    try: start = int(sys.argv[3])
+    except: pass
+    maxThreads = 200
+    try: maxThreads = int(sys.argv[4])
+    except: pass
+    print("Fetch every hash for passwordlist: range() | range: " + str(start) + "|" + str(maxThreads))
+    time.sleep(1)
+    
+    file = open(ftname, "a", encoding="utf-8")
+    c = 1
+    c_ = 1
+    for v in range(start, maxThreads):
+        st = "a"*v
+        for i in "abcdefghijklmnopqrstuvwxyz":
+            st_ = st + i
+            val_h = hashFunction.hexHash(st_)
+            print(c, st_, val_h)
+            file.write(val_h + "\n")
+            c_+=1
+        c+=1
+    file.close()
+    print("Fetch done. " + str(c-1) + "|" + str(c_-1) + " strings hashed")
+elif w.startswith("-collision"):
+    fname = sys.argv[2]
+    print("Check hashlist for collisions: " + fname)
+    time.sleep(1)
+    
+    file = open(fname, "r", encoding="utf-8")
+    lines = file.readlines()
+    file.close()
+    
+    c = 1
+    for val in lines:
+        print(c, val.replace("\n", ""))
+        if lines.count(val) > 1:
+            print("=> collision found:", c, val.replace("\n", ""), "\n=> " + str(lines.count(val)) + " times")
+            quit()
+        c+=1
+    print("No collisions found :)")
+else:
+    print("input       :", w)
+    print("hash        :", hashFunction.hexHash(w, False, True))
+
+end_time = time.time()
+print("process time:", str(round(end_time-start_time, 6)) + "sec")
